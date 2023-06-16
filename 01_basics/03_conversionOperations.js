53e7a27645d9c1d8e0a0c32bafbbe21a81f36134
Mohd Sultan <132590302+MohdSultanGit@users.noreply.github.com>
2023-06-16 04:49:11 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+let score = "Sultan"
+
+// console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = "Sultan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn );
+
+// 1=> true; 0 => false
+// "" => false
+// "sultan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
