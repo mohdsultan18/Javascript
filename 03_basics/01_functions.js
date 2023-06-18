b918fc890fe5060cd653cbda3f9bc0ce2f3a4693
Mohd Sultan <132590302+MohdSultanGit@users.noreply.github.com>
2023-06-18 13:41:38 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,4 +36,35 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("sultan"));
-console.log(loginUserMessage("sultan"));
+// console.log(loginUserMessage("sultan"));
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: 'sultan'
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(mynewArray));
+console.log(returnSecondValue(200, 400, ));
