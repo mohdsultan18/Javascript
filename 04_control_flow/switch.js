ef5dd3c06d4ab5bedce50f1232abe35503f3e76c
Mohd Sultan <132590302+MohdSultanGit@users.noreply.github.com>
2023-06-19 06:16:53 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Febuary");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
