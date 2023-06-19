a0142ee80bfc91eb046339935ceb7a3db6b72ace
Mohd Sultan <132590302+MohdSultanGit@users.noreply.github.com>
2023-06-19 04:10:51 +0000

    scope level and mini

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,9 +3,56 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-     console.log("INNER: ", a);
+    //  console.log("INNER: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+
+
+function one(){
+    const username = "sultan"
+
+    function two (){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    // two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "sultan"
+    if (username === "sultan") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ************************* Interesting *****************************
+
+
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
